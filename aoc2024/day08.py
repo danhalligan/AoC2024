a3c7827f876f06afb332c00b22b52b78ddf65e78
Dan Halligan <dan.halligan@gmail.com>
2024-12-08 08:29:29 +0000

    TIdying

--- aoc2024/day08.py
+++ aoc2024/day08.py
@@ -1,50 +1,33 @@
-from itertools import combinations
+from itertools import permutations
 
 
 def parse(data):
-    lines = data.lines()
-    return (
-        {
-            complex(i, j): v
-            for j, line in enumerate(data.lines())
-            for i, v in enumerate(list(line))
-            if v != "."
-        },
-        complex(len(lines), len(lines[0])),
-    )
-
-
-def inbound(x, lim):
-    return 0 <= x.real < lim.real and 0 <= x.imag < lim.imag
+    return {
+        complex(i, j): v
+        for j, line in enumerate(data.lines())
+        for i, v in enumerate(list(line))
+    }
 
 
 def part_a(data):
-    grid, lim = parse(data)
+    grid = parse(data)
     antipoles = []
-    for ant in set(grid.values()):
-        locations = [k for k, v in grid.items() if v == ant]
-        for a, b in combinations(locations, 2):
-            dist = b - a
-            antipoles += [a - dist, b + dist]
+    for freq in {*grid.values()} - {"."}:
+        locations = [k for k in grid if grid[k] == freq]
+        antipoles += [b + (b - a) for a, b in permutations(locations, 2)]
 
-    antipoles = [x for x in antipoles if inbound(x, lim)]
-    return len(set(antipoles))
+    return len(set(antipoles) & set(grid))
 
 
 def part_b(data):
-    grid, lim = parse(data)
+    grid = parse(data)
     antipoles = []
-    for ant in set(grid.values()):
-        locations = [k for k, v in grid.items() if v == ant]
-        for a, b in combinations(locations, 2):
-            dist = b - a
-            pos = a
-            while inbound(pos, lim):
-                antipoles += [pos]
-                pos -= dist
+    for freq in {*grid.values()} - {"."}:
+        locations = [k for k in grid if grid[k] == freq]
+        for a, b in permutations(locations, 2):
             pos = b
-            while inbound(pos, lim):
+            while pos in grid:
                 antipoles += [pos]
-                pos += dist
+                pos += b - a
 
     return len(set(antipoles))
