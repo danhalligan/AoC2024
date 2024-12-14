b7642551ca186f41e88e49561cdd83ea2981cf02
Dan Halligan <dan.halligan@gmail.com>
2024-12-14 14:38:57 +0000

    Tidying

--- aoc2024/day12.py
+++ aoc2024/day12.py
@@ -1,6 +1,10 @@
+def moves(cell):
+    return [cell + m for m in [-1j, -1, 1, +1j]]
+
+
 def neighbours(grid, x):
     """valid neighbours of a cell"""
-    return [x + m for m in [1, +1j, -1j, -1] if x + m in grid]
+    return [m for m in moves(x) if m in grid]
 
 
 def flood(grid, cell):
@@ -16,52 +20,43 @@ def flood(grid, cell):
 
 
 def perimeter(grid, region):
-    perim = 0
-    for cell in region:
-        bounds = [cell + m for m in [1, +1j, -1j, -1]]
-        perim += len([x for x in bounds if x not in grid or grid[x] != grid[cell]])
-    return perim
+    return sum(
+        sum([x not in grid or grid[x] != grid[cell] for x in moves(cell)])
+        for cell in region
+    )
 
 
-def bounds(grid, cell):
+def regions(grid):
+    todo = set(grid.keys())
+    while todo:
+        region = flood(grid, todo.pop())
+        todo = todo - region
+        yield region
+
+
+def part_a(data):
+    grid = data.grid()
+    return sum(len(region) * perimeter(grid, region) for region in regions(grid))
+
+
+def borders(grid, cell):
     bounds = [1, +1j, -1j, -1]
-    return [x for x in bounds if cell + x not in grid or grid[cell + x] != grid[cell]]
+    return set(
+        [x for x in bounds if cell + x not in grid or grid[cell + x] != grid[cell]]
+    )
 
 
 def sides(grid, region):
     sides = 0
     for cell in region:
-        bnds = set(bounds(grid, cell))
-        lu = [cell + m for m in [-1j, -1]]
-        nbs = [x for x in lu if x in grid and grid[x] == grid[cell]]
-        for nb in nbs:
-            bnds -= set(bounds(grid, nb))
-        sides += len(bnds)
+        bounds = borders(grid, cell)
+        for nb in [cell + m for m in [-1j, -1]]:
+            if nb in grid and grid[nb] == grid[cell]:
+                bounds -= borders(grid, nb)
+        sides += len(bounds)
     return sides
-
-
-def regions(grid):
-    todo = list(grid.keys())
-    regions = []
-    while todo:
-        cell = todo[0]
-        region = flood(grid, cell)
-        todo = [x for x in todo if x not in region]
-        regions += [{"type": grid[cell], "cells": region}]
-    return regions
-
-
-def part_a(data):
-    grid = data.grid()
-    res = 0
-    for region in regions(grid):
-        res += len(region["cells"]) * perimeter(grid, region["cells"])
-    return res
 
 
 def part_b(data):
     grid = data.grid()
-    res = 0
-    for region in regions(grid):
-        res += len(region["cells"]) * sides(grid, region["cells"])
-    return res
+    return sum(len(region) * sides(grid, region) for region in regions(grid))
